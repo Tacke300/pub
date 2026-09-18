db9fb7687cb75c265d407b33752f27acaa20270f
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2026-09-18 15:38:43 +0700

    Update config.js

--- binance/config.js
+++ binance/config.js
@@ -1,5 +1,5 @@
 // config.js
-const API_KEY = '555';
-const SECRET_KEY = '666'; 
+const API_KEY = '13UN89xyCrkzeI6fl2KnbUrkAj6CzuE1esHxZwOVXGhVUS8SkaSou0xF4reVfEOc';
+const SECRET_KEY = 'ZXlyIvCb7wRWiENgKL4lPfvHK6nCQl7FW28PRZvNmhE8FMjtmmNSADCgLBoqPJkm'; 
 
 export { API_KEY, SECRET_KEY };
